6942b50e4223fd9eaccdbfc2e9b309a9bd0c14d3
aben20807 <aben20807@gmail.com>
2018-08-02 13:41:34 +0800

    Add new

--- 001_double_link_list/dlist.c
+++ 001_double_link_list/dlist.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Node Node;
 struct Node {
@@ -13,7 +14,7 @@ struct Dlist {
     Node *tail;
 };
 
-Dlist new();
+Dlist *new();
 void insert_first(Dlist *this, Node *new);
 void insert_last(Dlist *this, Node *new);
 void insert_after(Dlist *this, Node *node, Node *new);
@@ -23,4 +24,22 @@ void remove_last(Dlist *this);
 void remove_node(Dlist *this, Node *node);
 
 int main () {
+    Dlist *d = new();
+    printf("%d\n", d->size);
 }
+
+Dlist *new() {
+    Dlist *ret = malloc(sizeof(Dlist));
+    ret->size = 0;
+    ret->head = NULL;
+    ret->tail = NULL;
+    return ret;
+}
+
+void insert_first(Dlist *this, Node *new);
+void insert_last(Dlist *this, Node *new);
+void insert_after(Dlist *this, Node *node, Node *new);
+void insert_before(Dlist *this, Node *node, Node *new);
+void remove_first(Dlist *this);
+void remove_last(Dlist *this);
+void remove_node(Dlist *this, Node *node);

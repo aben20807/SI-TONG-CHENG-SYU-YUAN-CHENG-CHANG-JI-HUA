76304ac1da68ccd37971b6a1081d6362bc7fc38d
aben20807 <aben20807@gmail.com>
2018-08-04 21:58:31 +0800

    Return status

--- 002_double_link_list_myenc/dlist.h
+++ 002_double_link_list_myenc/dlist.h
@@ -4,14 +4,15 @@
 #include <stddef.h>
 
 typedef struct _DList DList;
+typedef enum {OK, ERR} Status;
 
 DList *dlist_create(void);
-void dlist_insert(DList *this, size_t index, void *data);
-void dlist_prepend(DList *this, void *data);
-void dlist_append(DList *this, void *data);
-void dlist_delete(DList *this, size_t index);
-void dlist_get_by_index(DList *this, size_t index, void **data);
-void dlist_set_by_index(DList *this, size_t index, void *data);
-size_t dlist_length(DList *this);
+Status dlist_insert(DList *this, size_t index, void *data);
+Status dlist_prepend(DList *this, void *data);
+Status dlist_append(DList *this, void *data);
+Status dlist_delete(DList *this, size_t index);
+Status dlist_get_by_index(DList *this, size_t index, void **data);
+Status dlist_set_by_index(DList *this, size_t index, void *data);
+size_t dlist_size(DList *this);
 
 #endif

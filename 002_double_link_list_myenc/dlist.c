da4b686cd35c10d8b5f1178df85c6831136d564c
aben20807 <aben20807@gmail.com>
2018-08-04 23:37:21 +0800

    Remove macros, finished insert and append at first and end

--- 002_double_link_list_myenc/dlist.c
+++ 002_double_link_list_myenc/dlist.c
@@ -1,15 +1,6 @@
 #include <stdlib.h>
 #include "dlist.h"
 
-#define NULL_CHECK(ptr) \
-    do { \
-        if (ptr == NULL) ERR; \
-    } while(0)
-#define INDEX_CHECK(list, index) \
-    do { \
-        if (index >= list->size) ERR; \
-    } while(0)
-
 typedef struct _Node {
     void *data;
     struct _Node *pre;
@@ -22,30 +13,6 @@ struct _DList {
     Node *tail;
 };
 
-DList *dlist_create(void)
-{
-    DList *ret = malloc(sizeof(DList));
-    ret->size = 0;
-    ret->head = NULL;
-    ret->tail = NULL;
-    return ret;
-}
-
-Status dlist_insert(DList *this, size_t index, void *data)
-{
-    NULL_CHECK(this);
-    int cnt = 0;
-    Node *cur = this->head;
-    return ERR;
-}
-
-Status dlist_prepend(DList *this, void *data);
-Status dlist_append(DList *this, void *data);
-Status dlist_delete(DList *this, size_t index);
-Status dlist_get_by_index(DList *this, size_t index, void **data);
-Status dlist_set_by_index(DList *this, size_t index, void *data);
-size_t dlist_size(DList *this);
-
 static Node *node_create(void *data)
 {
     Node *ret = malloc(sizeof(Node));
@@ -57,8 +24,9 @@ static Node *node_create(void *data)
 
 static Node *node_at(DList *this, const int index)
 {
-    NULL_CHECK(this);
-    INDEX_CHECK(this, index);
+    if (this == NULL || index >= this->size) {
+        return NULL;
+    }
     Node *itr = this->head;
     int cnt = 0;
     while (itr != NULL) {
@@ -69,3 +37,68 @@ static Node *node_at(DList *this, const int index)
     }
     return NULL;
 }
+
+DList *dlist_create(void)
+{
+    DList *ret = malloc(sizeof(DList));
+    ret->size = 0;
+    ret->head = NULL;
+    ret->tail = NULL;
+    return ret;
+}
+
+Status dlist_insert(DList *this, size_t index, void *data)
+{
+    Node *new = node_create(data);
+    if (new == NULL) {
+        return ERR;
+    }
+    if (index == 0) {
+        if (this->head == NULL) {
+            this->head = new;
+            this->tail = this->head;
+        } else {
+            this->head->pre = new;
+            new->nxt = this->head;
+            this->head = new;
+        }
+        return OK;
+    } else if (index == this->size) {
+        if (this->tail == NULL) {
+            this->tail = new;
+            this->head = this->head;
+        } else {
+            this->tail->nxt = new;
+            new->pre = this->tail;
+            this->tail = new;
+        }
+        return OK;
+    } else if (0 < index && index < this->size) {
+        Node *target = node_at(this, index);
+        if (target != NULL) {
+            target->pre->nxt = new;
+            new->pre = target->pre;
+            new->nxt = target;
+            target->pre = new;
+            this->size++;
+            return OK;
+        }
+    } else {
+        return ERR;
+    }
+}
+
+Status dlist_prepend(DList *this, void *data)
+{
+    return dlist_insert(this, 0, data);
+}
+
+Status dlist_append(DList *this, void *data)
+{
+    return dlist_insert(this, this->size, data);
+}
+
+Status dlist_delete(DList *this, size_t index);
+Status dlist_get_by_index(DList *this, size_t index, void **data);
+Status dlist_set_by_index(DList *this, size_t index, void *data);
+size_t dlist_size(DList *this);

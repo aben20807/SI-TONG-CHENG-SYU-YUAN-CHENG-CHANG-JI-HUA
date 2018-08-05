1a4a9d987d0b437957489d8f5b501ab934dfae33
aben20807 <aben20807@gmail.com>
2018-08-06 00:01:43 +0800

    Add question 001

---
+++ 999_questions/001_code_reuse/v3.c
@@ -0,0 +1,78 @@
+Status dlist_insert(DList *this, size_t index, void *data)
+{
+    Node *new = node_create(data);
+    if (this == NULL || data == NULL || new == NULL) {
+        return ERR;
+    }
+    if (index == 0) {
+        if (this->head == NULL) {
+            this->head = new;
+            this->tail = this->head;
+        } else {
+            this->head->pre = new;
+            new->nxt = this->head;
+            this->head = new;
+        }
+        this->size++;
+        return OK;
+    } else if (index == this->size) {
+        if (this->tail == NULL) {
+            this->tail = new;
+            this->head = this->head;
+        } else {
+            this->tail->nxt = new;
+            new->pre = this->tail;
+            this->tail = new;
+        }
+        this->size++;
+        return OK;
+    } else if (0 < index && index < this->size) {
+        Node *target = node_at(this, index);
+        if (target != NULL) {
+            target->pre->nxt = new;
+            new->pre = target->pre;
+            new->nxt = target;
+            target->pre = new;
+            this->size++;
+            return OK;
+        }
+    } else {
+        return ERR;
+    }
+}
+
+Status dlist_prepend(DList *this, void *data)
+{
+    Node *new = node_create(data);
+    if (this == NULL || data == NULL || new == NULL) {
+        return ERR;
+    }
+    if (this->head == NULL) {
+        this->head = new;
+        this->tail = this->head;
+    } else {
+        this->head->pre = new;
+        new->nxt = this->head;
+        this->head = new;
+    }
+    this->size++;
+    return OK;
+}
+
+Status dlist_append(DList *this, void *data)
+{
+    Node *new = node_create(data);
+    if (this == NULL || data == NULL || new == NULL) {
+        return ERR;
+    }
+    if (this->tail == NULL) {
+        this->tail = new;
+        this->head = this->head;
+    } else {
+        this->tail->nxt = new;
+        new->pre = this->tail;
+        this->tail = new;
+    }
+    this->size++;
+    return OK;
+}

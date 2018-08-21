8e32713a48c2e03bc00a00d17465785b3f601b77
aben20807 <aben20807@gmail.com>
2018-08-21 11:32:49 +0800

    Add typedefs

--- 006_double_link_list_pthread/dlist.h
+++ 006_double_link_list_pthread/dlist.h
@@ -1,41 +1,25 @@
 #ifndef _DLIST_H
 #define _DLIST_H
 
-#include <stddef.h>
-#include <stdbool.h>
+#include "locker.h"
+#include "typedefs.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+DECLS_BEGIN
 
 typedef struct _DList DList;
-typedef enum {OK, ERR} DListRet;
-typedef DListRet (*DListVisitFunc)(void *ctx, void *data, bool is_first);
+typedef Ret (*DListVisitFunc)(void *ctx, void *data, bool is_first);
 
 DList *dlist_create(void);
 void dlist_destroy(DList* thiz);
-DListRet dlist_insert(DList *thiz, size_t index, void *data);
-DListRet dlist_prepend(DList *thiz, void *data);
-DListRet dlist_append(DList *thiz, void *data);
-DListRet dlist_delete(DList *thiz, size_t index);
-DListRet dlist_get_by_index(DList *thiz, size_t index, void **data);
-DListRet dlist_set_by_index(DList *thiz, size_t index, void *data);
+Ret dlist_insert(DList *thiz, size_t index, void *data);
+Ret dlist_prepend(DList *thiz, void *data);
+Ret dlist_append(DList *thiz, void *data);
+Ret dlist_delete(DList *thiz, size_t index);
+Ret dlist_get_by_index(DList *thiz, size_t index, void **data);
+Ret dlist_set_by_index(DList *thiz, size_t index, void *data);
 size_t dlist_size(const DList *thiz);
-DListRet dlist_foreach(DList *thiz, DListVisitFunc visit, void *ctx);
+Ret dlist_foreach(DList *thiz, DListVisitFunc visit, void *ctx);
 
-#define return_if_fail(p) \
-    if(!(p)) { \
-        printf("%s:%d Warning: "#p" failed.\n", __func__, __LINE__); \
-        return; \
-    }
-#define return_val_if_fail(p, ret) \
-    if(!(p)) { \
-        printf("%s:%d Warning: "#p" failed.\n", __func__, __LINE__); \
-        return (ret); \
-    }
+DECLS_END
 
-#ifdef __cplusplus
-}
-#endif
-
-#endif
+#endif /* _DLIST_H */
---
+++ 006_double_link_list_pthread/typedefs.h
@@ -0,0 +1,32 @@
+#ifndef _TYPEDEFS_H
+#define _TYPEDEFS_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef enum _Ret {
+    OK,
+    ERR,
+} Ret;
+
+#ifdef __cplusplus
+#define DECLS_BEGIN extern "C" {
+#define DECLS_END   }
+#else
+#define DECLS_BEGIN
+#define DECLS_END
+#endif /* __cplusplus */
+
+#define return_if_fail(p) \
+    if(!(p)) { \
+        printf("%s:%d Warning: "#p" failed.\n", __func__, __LINE__); \
+        return; \
+    }
+#define return_val_if_fail(p, ret) \
+    if(!(p)) { \
+        printf("%s:%d Warning: "#p" failed.\n", __func__, __LINE__); \
+        return (ret); \
+    }
+
+
+#endif /* _TYPEDEFS_H */

5d8e340fca42e2c982e0b2830131c410e059eed0
aben20807 <aben20807@gmail.com>
2018-08-21 12:10:42 +0800

    Add call function

--- 006_double_link_list_pthread/locker.h
+++ 006_double_link_list_pthread/locker.h
@@ -1,6 +1,8 @@
 #ifndef _LOCKER_H
 #define _LOCKER_H
 
+#include "typedefs.h"
+
 DECLS_BEGIN
 
 typedef Ret (*LockerLockFunc)(Locker *thiz);
@@ -15,6 +17,25 @@ struct _Locker {
     char priv[0];
 }
 
+static inline Ret locker_lock(Locker *thiz)
+{
+    return_val_if_fail(thiz != NULL && thiz->lock != NULL, ERR);
+    return thiz->lock(thiz);
+}
+
+static inline Ret locker_unlock(Locker *thiz)
+{
+    return_val_if_fail(thiz != NULL && thiz->unlock != NULL, ERR);
+    return thiz->unlock(thiz);
+}
+
+static inline void locker_destroy(Locker *thiz)
+{
+    return_val_if_fail(thiz != NULL && thiz->lock != NULL, ERR);
+    thiz->destroy(thiz);
+    return;
+}
+
 DECLS_END
 
-#endif
+#endif /* _LOCKER_H */

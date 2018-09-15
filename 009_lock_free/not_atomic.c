7bd21fa49c5edb92daf6f778635374b2e5141f16
aben20807 <aben20807@gmail.com>
2018-09-15 11:02:16 +0800

    Add 009 and create not atomic version

---
+++ 009_lock_free/not_atomic.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define TIMES int _i = 0; _i < 100000; _i++
+
+void *thread_inc(void *count)
+{
+    for (TIMES) {
+        (*(int *)count)++;
+    }
+}
+
+void *thread_dec(void *count)
+{
+    for (TIMES) {
+        (*(int *)count)--;
+    }
+}
+
+int main(void){
+
+    int count = 0;
+    pthread_t inc_tid = 0;
+	pthread_t dec_tid = 0;
+
+	pthread_create(&inc_tid, NULL, thread_inc, &count);
+	pthread_create(&dec_tid, NULL, thread_dec, &count);
+
+	pthread_join(inc_tid, NULL);
+	pthread_join(dec_tid, NULL);
+
+    printf("%d\n", count);
+    return 0;
+}
